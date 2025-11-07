241821e0e37050c31780b15986966d0c1f75edd5
ScanToys <forack@outlook.com>
2025-11-07 17:57:35 +0800

    添加🛒Buy Now

--- locale.bat
+++ locale.bat
@@ -1,29 +1,29 @@
-cd ../Manual/Platform
-sphinx-build -b gettext source ../../Translate/Platform
+cd ../../Workspace/scanner-document/Platform
+sphinx-build -b gettext source ../../../Document/manual-local-crowdin1/Platform
 
 cd ../Reader
-sphinx-build -b gettext source ../../Translate/Reader
+sphinx-build -b gettext source ../../../Document/manual-local-crowdin1/Reader
 
 cd ../Wired-212
-sphinx-build -b gettext source ../../Translate/Wired-212
+sphinx-build -b gettext source ../../../Document/manual-local-crowdin1/Wired-212
 
 cd ../Wired-280
-sphinx-build -b gettext source ../../Translate/Wired-280
+sphinx-build -b gettext source ../../../Document/manual-local-crowdin1/Wired-280
 
 cd ../Wired-290
-sphinx-build -b gettext source ../../Translate/Wired-290
+sphinx-build -b gettext source ../../../Document/manual-local-crowdin1/Wired-290
 
 cd ../Wired-CCD
-sphinx-build -b gettext source ../../Translate/Wired-CCD
+sphinx-build -b gettext source ../../../Document/manual-local-crowdin1/Wired-CCD
 
 cd ../Wired-DP
-sphinx-build -b gettext source ../../Translate/Wired-DP
+sphinx-build -b gettext source ../../../Document/manual-local-crowdin1/Wired-DP
 
 cd ../Wired-HS7
-sphinx-build -b gettext source ../../Translate/Wired-HS7
+sphinx-build -b gettext source ../../../Document/manual-local-crowdin1/Wired-HS7
 
 cd ../Wireless
-sphinx-build -b gettext source ../../Translate/Wireless
+sphinx-build -b gettext source ../../../Document/manual-local-crowdin1/Wireless
 
 cd ../Wireless-WiFi
-sphinx-build -b gettext source ../../Translate/Wireless-WiFi
+sphinx-build -b gettext source ../../../Document/manual-local-crowdin1/Wireless-WiFi
